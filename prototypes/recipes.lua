5cd54221b184b86ba2051c22275bed6b7015c208
Scott Sampson <scottsampson@yahoo.co.uk>
2017-05-15 18:31:52 +0100

    Added Modified Steel Recipe

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,2 +1,25 @@
 data.raw.recipe["rail"].ingredients[1][1] = "stone-brick";
-data.raw.recipe["concrete"].ingredients[2][1] = "iron-stick"
+data.raw.recipe["concrete"].ingredients[2][1] = "iron-stick";
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "steel-plate",
+    category = "smelting",
+    normal =
+    {
+      enabled = false,
+      energy_required = 17.5,
+      ingredients = {{"iron-plate", 2}},
+      result = "steel-plate"
+    },
+    expensive =
+    {
+      enabled = false,
+      energy_required = 35,
+      ingredients = {{"iron-plate", 10}},
+      result = "steel-plate"
+    }
+  }
+}
+)
